5f9fb339841e1a3add38d832e736204dea4185f8
fortun3r <aytosin80@gmail.com>
2023-03-16 11:25:13 -0700

    Another commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * Main: The entry point
+ * 
+ * Return: Always return 0 if code runs successfully
+ */
+int main()
+{
+    char;
+    int;
+    long int;
+    long long int;
+    float;
+    printf("Size of a char: %i byte(s)",sizeof(char));
+    printf("Size of an int: %i byte(s)",sizeof(int));
+    printf("Size of a long int: %i",sizeof(long int));
+    printf("Size of a long long int: %i byte(s)",sizeof(long long int));
+    printf("Size of a float: %i byte(s)",sizeof(float));
+    return 0;
+
+}

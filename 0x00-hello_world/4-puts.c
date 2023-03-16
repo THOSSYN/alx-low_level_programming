0f0f4aa619d0bb931eccc940f0a47d7ba9bcb768
fortun3r <aytosin80@gmail.com>
2023-03-16 12:12:00 -0700

    types in byte

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints "Programming is like building a multilingual puzzle, followed by a newline
- *
+ * main - prints "Programming is like building a multilingual puzzle,
+ * followed by a newline
  * Return: Always 0 (Success)
  */
 int main(void)

de7dba0fc6cdb1f361c5336e5e1073fe898eb7ee
fortun3r <aytosin80@gmail.com>
2023-03-16 09:00:44 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Main: the entry point
+ *
+ * Return: Always return 0 if programme runs successfully
+ */
+
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return 0;
+}

6dcc7b7160de8da3aa1c2dafc7d15ec8ef37425b
fortun3r <aytosin80@gmail.com>
2023-03-16 14:21:43 -0700

    Well to commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
- * main -  prints the size of various types on the computer
- * it is compiled and run on.
- * Return: Always 0 
+ *main - "prints the size of various types on the computer it is 
+ compiled and run on."
+ *
+ *Return: Always 0 
  */
 int main(void)
 {

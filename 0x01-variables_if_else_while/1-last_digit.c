149de6ff38c3dab674c06db64a46a3e692a8dc55
fortun3r <aytosin80@gmail.com>
2023-03-17 05:45:59 -0700

    My first commit today

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <math.h>
+
+/**
+ *main - "assigns a random number but print the last digit."
+ *
+ *Return Always 0
+ */
+
+
+int main(void)
+{
+	int n, last_dig;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last_dig = n % 10;
+	
+	if (last_dig == 0){
+		printf("Last digit of %d is %d and is equal to 0\n", n, last_dig);
+	}
+	else if (last_dig > 0){
+		printf("Last digit of: %d is: %d and is greater than 5\n", n, last_dig);
+	}
+	else{
+		printf("Last digit of: %d is: %d and is less than 6 and not 0\n", n, last_dig);
+	}
+	return (0);
+}
+
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+#This project is based on variable if else and while control mechanism

3651ca83cbd38bf1079a240ef8514cfa620e92a7
fortun3r <aytosin80@gmail.com>
2023-03-17 11:23:50 -0700

    Counting

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,9 +11,9 @@ int main(void)
 	char x;
 	char y;
 
-	for ( x = 'a'; x <= 'z'; x++)
+	for (x = 'a'; x <= 'z'; x++)
 		putchar(x);
-			for( y = 'A'; y <= 'Z'; y++)
+			for (y = 'A'; y <= 'Z'; y++)
 				putchar(y);
 		putchar('\n');
 	return (0);

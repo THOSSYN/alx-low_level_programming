aef6c44729b30871fcbb3ac4d9844a23546300c1
fortun3r <aytosin80@gmail.com>
2023-03-17 12:24:26 -0700

    print

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,15 +10,14 @@ int main(void)
 {
 	char c;
 
-	for(c ='a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
-		if(c == 'e' || c == 'q')
+		if (c == 'e' || c == 'q')
 		{
 			continue;
 		}
 		putchar(c);
 	}
 	putchar('\n');
-	
 	return (0);
 }

bc5d694ed2041332f6d1c9441252bc567aed17ec
fortun3r <aytosin80@gmail.com>
2023-03-21 15:44:20 -0700

    Add function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *add function - "add two numbers together."
+ *
+ *Return Always 0.
+ */
+
+int add(int x, int y)
+{
+	int sum;
+	sum = (x + y);
+	return sum;
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ *main - "checks the code."
+ *
+ *Return: Always 0.
+ */
+
+int main(void)
+{
+	int a, b, c;
+	a = 89;
+	b = 9;
+	c = add(a,b);
+	printf("%d\n", c);
+	return (0);
+}

c78169aa7e64631e39e1d155c333c91350204b6d
fortun3r <aytosin80@gmail.com>
2023-03-21 16:44:41 -0700

    recommitting 11-print

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,12 +3,14 @@
 /**
  *print_to_98 - "prints natural number n to 98."
  *
+ *n: is the initial value.
+ *
  *Return: Always 0
  */
 
 void print_to_98(int n)
 {
-	for (n = -10; n <= 98; n++)
+	for (n = 0; n <= 98; n++)
 	{
 		printf("%d", n);
 		if (n == 98)

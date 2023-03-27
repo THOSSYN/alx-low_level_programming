40c21a6208edcf40c8cf25189e27c63e5053320b
fortun3r <aytosin80@gmail.com>
2023-03-27 14:32:00 -0700

    strcpy

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,6 +7,8 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 int main(void);
 #endif

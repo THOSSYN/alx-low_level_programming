9dcccb010997239606ccd53cd36d13040f779143
fortun3r <aytosin80@gmail.com>
2023-04-17 10:05:33 -0700

    Working preprocessor

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - prints the name of the file from which
+ *it compile from.
+ *Return: 0 on program success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}

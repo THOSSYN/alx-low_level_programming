67d80c3c1a6d081912af608f3b791a9005bd09d0
THOSSYN <mtafortunefinder@gmail.com>
2023-07-07 01:48:59 -0700

    Corrected: 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,6 +11,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hash_t;
 	unsigned long int i;
 
+	if (size == 0)
+		return (NULL);
+
 	hash_t = malloc(sizeof(hash_table_t));
 	if (!hash_t)
 	{
@@ -19,7 +22,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_t->size = size;
 	hash_t->array = malloc(size * sizeof(hash_node_t *));
 	if (hash_t->array == NULL)
+	{
+		free(hash_t);
 		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 		hash_t->array[i] = NULL;
 

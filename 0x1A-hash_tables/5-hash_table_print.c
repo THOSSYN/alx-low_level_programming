e4a3e0aed5ee10606eff4334fef6056033e1970b
THOSSYN <mtafortunefinder@gmail.com>
2023-07-07 02:57:40 -0700

    Updated 5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,27 +1,31 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_print - prints hash table
+ * @ht: is pointer to hash table
+ * Return: nothing
+ */
 void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *current;
-	unsigned long int i, printed;
+	unsigned long int i;
+	int printdelim;
 
 	if (ht == NULL)
 		return;
 
 	printf("{");
-	printed = 0;
+	printdelim = 0;
 	for (i = 0; i < ht->size; i++)
 	{
 		current = ht->array[i];
 		while (current != NULL)
 		{
-			printf("'%s' : '%s'", current->key, current->value);
-			current = current->next;
-			if (current != NULL)
-			{
+			if (printdelim)
 				printf(", ");
-			}
-			printed++;
+			printf("'%s' : '%s'", current->key, current->value);
+			printdelim = 1;
+			current = current->next;
 		}
 	}
 	printf("}\n");

40d5d664245ee4f6d5b6de1a9401f1586be49768
THOSSYN <mtafortunefinder@gmail.com>
2023-10-10 02:06:49 -0700

    Corrected 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -2,6 +2,37 @@
 #include "search_algos.h"
 
 /**
+ * print - A function that displays the output of an array
+ * @array: is the list of element
+ * @size: is the length of the list
+ * @delim: is a 0 or 1 value to set flag
+ * Return: nothing
+ */
+
+void print(int *array, size_t size, int delim)
+{
+	size_t i = 0;
+
+	if (size > 0)
+	{
+		printf("Searching in array: ");
+		for (i = 0; i < size; i++)
+		{
+			if (delim)
+			{
+				printf(", %d", array[i]);
+			}
+			else
+			{
+				printf("%d", array[i]);
+				delim = 1;
+			}
+		}
+		printf("\n");
+	}
+}
+
+/**
  * binary_search - A function that searches for a value in a
  * sorted array
  * @array: is the list of element
@@ -12,17 +43,16 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	int mid, start, end, root, i;
+	int mid, start, end, root, delim;
+	size_t currentSize;
 
 	if (!value || array == NULL)
 		return (-1);
 
 	start = 0;
 	end = size - 1;
-	printf("Array: ");
-        for (i = start; i <= end; i++)
-		printf("%d ", array[i]);
-        printf("\n");
+	delim = 0;
+	print(array, size, delim);
 	while (start <= end)
 	{
 		mid = start + (end - start) / 2;
@@ -36,11 +66,9 @@ int binary_search(int *array, size_t size, int value)
 		else
 			start = mid + 1;
 
-		printf("Array: ");
-        	for (i = start; i <= end; i++)
-            		printf("%d ", array[i]);
-        	printf("\n");
+		currentSize = (value < root) ? (mid - start + 1) : (end - mid + 1);
+		print(array + start, currentSize - 1, delim);
+		delim = 0;
 	}
-
 	return (-1);
 }
